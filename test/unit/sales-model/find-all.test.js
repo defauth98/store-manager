6dbc7c0eb3e95c23a0ac1d4b2bd220789d103738
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-11-06 16:33:22 -0300

    testes do metodo findAll do sales model

---
+++ test/unit/sales-model/find-all.test.js
@@ -0,0 +1,77 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const mongoConnection = require('../../../src/model/connection');
+const salesModel = require('../../../src/model/salesModel');
+
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+describe("#salesModel", () => {
+  let connectionMock;
+
+  before(async () => {
+    const DBServer = new MongoMemoryServer();
+    const URLMock = await DBServer.getUri();
+
+    connectionMock = await MongoClient
+      .connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then((conn) => conn.db('model_example'));
+
+
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
+
+  describe("findAll", () => {
+    const sales = [
+      {
+        "productId": "6160267b2aa22b59a3012e2a",
+        "quantity": 2
+      }
+    ]
+
+    describe("when list is empty" , () => {
+      it("returns a array", async () => {
+        const response = await salesModel.findAll();
+
+        expect(response).to.be.a("array");
+      })
+
+      it("the array is empty", async () => {
+        const response = await salesModel.findAll();
+
+        expect(response.length).equals(0)
+      })
+    })
+
+    describe("when have a sales", () => {
+      before(async () => {
+        await salesModel.create(sales);
+      })
+
+      it("return a array of products", async () => {
+        const response = await salesModel.findAll();
+
+        expect(response.length).equals(1)
+      })
+
+      it('the product have an id', async () => {
+        const response = await salesModel.findAll();
+
+        expect(response[0]).to.have.property("_id");
+      })
+
+      it('the product have name and quantity', async () => {
+        const response = await salesModel.findAll();
+
+        expect(response[0]).to.have.property("_id");
+      })
+    })
+  })
+});
