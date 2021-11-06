29c4614683970f091ecf268f28d77b8fceeb18a3
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-11-06 16:40:32 -0300

    testes do metodo update do sales model

---
+++ test/unit/sales-model/update.test.js
@@ -0,0 +1,49 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const mongoConnection = require('../../../src/model/connection');
+const salesModel = require('../../../src/model/salesModel');
+
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+describe("#salesModel", () => {
+  let connectionMock;
+
+  before(async () => {
+    const DBServer = new MongoMemoryServer();
+    const URLMock = await DBServer.getUri();
+
+    connectionMock = await MongoClient
+      .connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then((conn) => conn.db('model_example'));
+
+
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
+
+  describe("update", () => {
+    const sales = [
+      {
+        "productId": "6160267b2aa22b59a3012e2a",
+        "quantity": 2
+      }
+    ]
+
+      it("returns a sale", async () => {
+        const sale = await salesModel.create(sales);
+
+        const updatedProduct = await salesModel.update(sale.id);
+
+        console.log(updatedProduct);
+
+        expect(updatedProduct).to.have.property("_id");
+      })
+    })
+});
